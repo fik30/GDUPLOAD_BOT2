4d4468a04bf463836a6b9b42f6ee387049c2c531
fik30 <95693615+fik30@users.noreply.github.com>
2022-08-15 22:41:47 +0700

    Update authorize.py

--- bot/plugins/authorize.py
+++ bot/plugins/authorize.py
@@ -14,7 +14,7 @@ from bot.helpers.utils import CustomFilters
 
 
 OAUTH_SCOPE = "https://www.googleapis.com/auth/drive"
-REDIRECT_URI = "http://localhost:1/"
+REDIRECT_URI = "com.googleusercontent.apps.123:redirect_uri_path"
 
 flow = None
 
