300c81f1afe444bb11af18b6bbda4f9e01f60261
fik30 <95693615+fik30@users.noreply.github.com>
2022-08-15 22:29:21 +0700

    Update authorize.py

--- bot/plugins/authorize.py
+++ bot/plugins/authorize.py
@@ -24,7 +24,7 @@ async def _auth(client, message):
   creds = gDriveDB.search(user_id)
   if creds is not None:
     creds.refresh(Http())
-    gDriveDB._set(creds)
+    gDriveDB._set(user_id, creds)
     await message.reply_text(Messages.ALREADY_AUTH, quote=True)
   else:
     global flow
